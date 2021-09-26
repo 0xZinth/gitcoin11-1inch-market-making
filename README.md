539f89889c69540b7e19d9484fab441eefebaea5
0xZinth <john.algo@outlook.com>
2021-09-26 10:33:50 +0900

    first

--- README.md
+++ README.md
@@ -1,61 +1,74 @@
-# OpenDefi-Hackathon-DHedge-Strat
+# Gitcoin Hackathon 11: 1Inch StableCoin Market Making
 
-This repo contains code for running two simple automated trading strategies using the dHEDGE platform. 
+This repo contains code for market making stable coins using the 1Inch limit order protocol. 
 
-- [Stable Coin Re-balancing](StrategyStables.md)
-- [Eth Trend Chasing](StrategyEth.md)
+Addresses this gitcoin [bounty](https://gitcoin.co/issue/1inch/limit-order-protocol/39/100026633).
 
-Addresses this gitcoin [bounty](https://gitcoin.co/issue/dhedge/dhedge-docs/4/100026363).
+## Description
+
+The script will market make each pair of USDC, DAI and USDT and take a simple assumption that each of these stables is worth the same. It is then always good  to swap one for more units of the other. Based on a basis points setting the script places all the possible order combinations offering to swap one for slightly more of the the other. For example with order size of 200 and basis point 2 we will swap 200 or token A for 200.04 fo token b (securing 4 cent profit with each swap!).
+
+Note the following implementation details:
+
+- All orders are placed with a 10 minute "time out" using the predicate feature. This means there is no need to cancel orders (which would cost gas). Generally this makes for easier testing and iteration as old orders always time out.
+- Over time the orders get filled so the script loops and adds new orders when the old orders remaining quantity gets low.
+
+### Results
+
+Ran this for a day and it does work well. I set the swap rate at 2 basis points and odder size at 200. At those settings I am seeing a few hits per hour (partial executions).
+
+Following is sample output. Shows the current coin balance in the account and the limit orders that are outstanding.
+
+```
+TotalBal:533.34, Token:USDC Balance:502.04, Token:DAI Balance:0.05, Token:USDT Balance:31.25
+From: USDC To: DAI remaining: 200.0 at create time:2021-09-25T12:41:23.581Z
+From: USDC To: USDT remaining: 200.0 at create time:2021-09-25T12:41:23.308Z
+From: DAI To: USDC remaining: 200.0 at create time:2021-09-25T12:36:17.639Z
+From: USDT To: USDC remaining: 200.0 at create time:2021-09-25T12:33:13.813Z
+From: USDT To: DAI remaining: 200.0 at create time:2021-09-25T12:33:12.972Z
+From: DAI To: USDT remaining: 200.0 at create time:2021-09-25T12:32:10.732Z
+```
+
 ## How to run
 
 - Clone this repository
 - Install Dependencies ```npm install```
 - [Setup the env file](#env-file-setup)
 
-Review the code and modify for your own purpose.
+Review the code and ensure you understand what it is doing.
 
-To run the strategies:
+To run: ```node main.js```
+### Comments on using the 1Inch API
 
-- Stable Swapping strategy ```node main.js```
-- Eth Trend Chaser strategy ```node main-eth.js```
+Some points about using the API
 
-### Comments on DHedge API
-
-Some points about using the DHedge API
-
-- I used the API to create the pool. I passed performance fee as 10 but it shows as 0 on the web site
-- There is no way to override gasLimit and gasPrice. For trading I inherited pool class and create override for trade function.
-  - Note I had to over ride gasLimit as my trade transactions were failing
-  - In polygon the gas price is volatile so I am overriding with fast gas.
-
+- I installed via npm. Had to edit the package.json file from src/index.js to index.js. Might be an issue with my environment.
+- Order predicate is listed as an optional field but orders submitted with no predicate were rejected. In any case I used predicate filed to time out my orders so this was not an issue in actual usage.
 ### .env file setup
 
 Rename `.env.sample` to `.env` and review/set the parameters:
 
-- RPC=https://polygon-rpc.com/
-- PRIVATE_KEY= Your Private Key Goes Here
-- ADDR=<add your address here>
-- STABLES_POOL=<strategy: dHedge pool address>
-- ETH_POOL=<strategy: dHedge pool address>
-- MAX_GAS=31
-- GAS_LIMIT=1000000
-- TRADE_SIZE=20
-- ETH_TRADE_SIZE=0.01
-- ETH_TRIGGER_BP=10
-- ETH_POLL_TIME=10
-- LOOP_TIME_MS=10000
-- NUM_OF_LOOPS=100000
+Note that MAX_GAS and GAS_LIMIT are not used.
 
-## Links to the strategys on dHEDGE
+```
+RPC=https://polygon-rpc.com/
+PRIVATE_KEY=<add you private key here>
+ADDR=<add your address here>
 
-- [Eth Trend Chaser](https://app.dhedge.org/manager/0x4ccd4510d7ae04dcc36e4bb541ebce5dc5023c34)
-- [Stable Swapping](https://app.dhedge.org/manager/0x32fab5a22b5545b9dff82f6db6c87434960955d3)
+MAX_GAS=31
+GAS_LIMIT=1000000
 
+TRADE_SIZE=200
+SPREADBP = 2
+
+LOOP_TIME_MS=60000
+NUM_OF_LOOPS=100000
+```
 ## Resources
 
-- [dHEDGE Docs](https://docs.dhedge.org/)
-- [Announcing dHEDGE V2](https://medium.com/dhedge-org/announcing-dhedge-v2-ed741b529b73)
-- [dHEDGE V2 SDK (npm)](https://www.npmjs.com/package/@dhedge/v2-sdk)
-- [dHEDGE V2 SDK (Medium)](https://medium.com/dhedge-org/dhedge-v2-sdk-d5d008fe255d)
-- [dHEDGE V2 SDK Example (boilerplate)](https://github.com/dhedge/dhedge-v2-sdk-examples)
-- [dHEDGE Discord](https://discord.gg/BAWTbRA)
+[Limit Order Protocol Documentation](https://docs.1inch.io/limit-order-protocol/)
+
+Use it right in [1inch dApp](https://app.1inch.io/#/1/classic/limit-order/WETH/DAI)
+Smart Contracts [repository](https://github.com/1inch/limit-order-protocol/)
+Utils library [repository](https://github.com/1inch/limit-order-protocol-utils/)
+REST API ([Ethereum](https://limit-orders.1inch.exchange/swagger/ethereum/), [BSC](https://limit-orders.1inch.exchange/swagger/binance/), [Polygon](https://limit-orders.1inch.exchange/swagger/polygon/))
--- main.js
+++ main.js
@@ -1,45 +1,106 @@
 const axios = require('axios')
-const { Dhedge, Dapp, Network, Transaction, Pool, ethers } = require("@dhedge/v2-sdk")
-const { denomToUnit, unitToDenom, TokensDB, findTkn } = require('./poly-utils.js')
-const IUniswapV2Router = require('./abi/IUniswapV2Router.json')
-const routerAddress = { [Network.POLYGON]: {  [Dapp.SUSHISWAP]: "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506"  } }
-
+const ethers = require('ethers')
+const Web3 = require('web3')
+const{ LimitOrderBuilder, LimitOrderProtocolFacade,  LimitOrderPredicateBuilder, PrivateKeyProviderConnector} = require('@1inch/limit-order-protocol')
+const IERC20ABI = require('@openzeppelin/contracts/build/contracts/IERC20.json')
+const LOPABI = require('@1inch/limit-order-protocol/abi/LimitOrderProtocol.json')
+const parseUnits = ethers.utils.parseUnits
+const formatUnits =ethers.utils.formatUnits
+const BN = ethers.BigNumber
 require('dotenv').config()
 
-const sushiPrice = require('./sushiPrice.js')
+const USDC = { "symbol": "USDC", "name": "USD Coin", "decimals": 6,
+    "address": "0x2791bca1f2de4661ed88a30c99a7a9449aa84174",
+    "logoURI": "https://tokens.1inch.io/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48.png"}
+const DAI = {"symbol": "DAI", "name": "Dai Stablecoin", "decimals": 18,
+     "address": "0x8f3cf7ad23cd3cadbd9735aff958023239c6a063",
+     "logoURI": "https://tokens.1inch.io/0x6b175474e89094c44da98b954eedeac495271d0f.png"}
+const USDT = {"symbol": "USDT", "name": "Tether USD", "decimals": 6,
+        "address": "0xc2132d05d31c914a87c6611c10748aeb04b58e8f",
+        "logoURI": "https://tokens.1inch.io/0xdac17f958d2ee523a2206206994597c13d831ec7.png"}
 
-//#region constants
+const symLookup = {'0x2791bca1f2de4661ed88a30c99a7a9449aa84174':USDC, '0x8f3cf7ad23cd3cadbd9735aff958023239c6a063':DAI, '0xc2132d05d31c914a87c6611c10748aeb04b58e8f':USDT}
 
+const address = process.env.ADDR
 const privateKey = process.env.PRIVATE_KEY
 const providerUrl = process.env.RPC
-
+const LOOP_TIME_MS = process.env.LOOP_TIME_MS
+const NUM_OF_LOOPS = process.env.NUM_OF_LOOPS
 const TRADE_SIZE = process.env.TRADE_SIZE
+const SPREADBP = process.env.SPREADBP// 1BP
+const DEFTIMEOUT = 600 //600 seconds is 10 minutes
+const contractAddress = '0xb707d89d29c189421163515c59e42147371d6857' //one Inch contract addr
+const chainId = 137
 
-const USDC = '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174'
-const DAI = '0x6B175474E89094C44Da98b954EedeAC495271d0F'
-const USDT = '0xc2132D05D31c914a87C6611C10748AEb04B58e8F'
+const curFormat = new Intl.NumberFormat('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })
 
-const MyPoolStables = process.env.STABLES_POOL
+const providerConnector = new PrivateKeyProviderConnector(privateKey,new Web3(providerUrl))
+const limitOrderBuilder = new LimitOrderBuilder(contractAddress, chainId, providerConnector)
+const limitOrderProtocolFacade = new LimitOrderProtocolFacade(contractAddress, providerConnector)
 
-//#endregion
+const limitOrderPredicateBuilder = new LimitOrderPredicateBuilder(limitOrderProtocolFacade)
+const { or, and, timestampBelow, nonceEquals, gt, lt, eq } = limitOrderPredicateBuilder
 
-//#region dhedge related
+async function createLimitOrder(fromT, toT, fromAmt, toAmt, livetime) { //live time in seconds
 
-class myPool extends Pool {
+    let simplePredicate = timestampBelow(Math.round(Date.now() / 1000) + livetime)
 
-    constructor(network, signer, poolLiogic, managerLogic, utils){super(network, signer, poolLiogic, managerLogic, utils)}
-
-    async trade( dapp, assetFrom, assetTo, amountIn, minAmountOut, gasPrice) {
-        const iUniswapV2Router = new ethers.utils.Interface(IUniswapV2Router.abi)
-        const swapTxData = iUniswapV2Router.encodeFunctionData(
-            Transaction.SWAP, [amountIn, minAmountOut, [assetFrom, assetTo], this.address, Math.floor(Date.now() / 1000) + 60 * 20  ])
-        return await this.poolLogic.execTransaction( routerAddress[this.network][dapp], swapTxData, {gasPrice, gasLimit:process.env.GAS_LIMIT} )
+    let order = limitOrderBuilder.buildLimitOrder({
+        makerAssetAddress:fromT.address,
+        takerAssetAddress: toT.address,
+        makerAddress: address,
+        makerAmount: ethers.utils.parseUnits(fromAmt, fromT.decimals).toString(),
+        takerAmount: ethers.utils.parseUnits(toAmt, toT.decimals).toString(),
+        predicate: simplePredicate,
+        //permit: '0x',
+        //interaction: '0x',
+    })
+    
+    let limitOrderTypedData = limitOrderBuilder.buildLimitOrderTypedData(order)
+    let limitOrderSignature = await providerConnector.signTypedData(address, limitOrderTypedData)
+    let limitOrderHash = limitOrderBuilder.buildLimitOrderHash( limitOrderTypedData)
+    
+    postData = {
+        orderHash: limitOrderHash,
+        orderMaker: address,
+        createDateTime: new Date(),
+        signature: limitOrderSignature,
+        makerAmount: ethers.utils.parseUnits(fromAmt, fromT.decimals).toString(),
+        takerAmount: ethers.utils.parseUnits(toAmt, toT.decimals).toString(),
+        data: limitOrderTypedData.message  
     }
+
+    res = await axios.post('https://limit-orders.1inch.exchange/v1.0/137/limit-order/', postData)
+    
+    return res.data
 }
 
-//#endregion
+async function getOpenLimitOrders() {
+    return (await axios.get(`https://limit-orders.1inch.exchange/v1.0/137/limit-order/address/${address}?page=1&limit=100&statuses=%5B1%2C2%5D`)).data
+}
 
-//#region utility functions
+async function getEnv(privateKey) {
+    let eProvider = new ethers.providers.JsonRpcProvider(providerUrl)
+    let acctWallet = new ethers.Wallet(privateKey)
+    let eSigner = acctWallet.connect(eProvider)
+    return {eProvider, eSigner}
+}
+
+async function getC(addr, abi, env) {
+    return new ethers.Contract(addr, abi, env.eSigner)
+}
+
+async function approveT(token, target, env) {
+    let tokenC = await getC(token.address,IERC20ABI.abi, env)
+    return await tokenC.approve(target,ethers.constants.MaxUint256,{gasPrice:'5000000000', gasLimit:'1000000'} )
+
+}
+
+async function getBal(token, env) {
+    const tokenC = await getC(token.address,IERC20ABI.abi, env)
+    const bal= await tokenC.balanceOf(env.eSigner.getAddress())
+    return parseFloat(ethers.utils.formatUnits(bal, token.decimals).toString())
+}
 
 async function getFastGas() {
     let httpData = await axios.get('https://gasstation-mainnet.matic.network/')
@@ -48,110 +109,61 @@ async function getFastGas() {
     return ethers.utils.parseUnits(fastMax.toString(), 'gwei')
 }
 
-
-function decimalize(amount, decimals) {
-    return JSBI.multiply(JSBI.BigInt(amount), JSBI.exponentiate(JSBI.BigInt(10), JSBI.BigInt(decimals)))
+async function getBalsAndDesc(tkns, env) {
+    const wBal =  await Promise.all(tkns.map(async tkn => {return {tkn, bal:await getBal(tkn, env)}}))
+    const totalBal = wBal.reduce((prev, cur) => prev = prev+cur.bal, 0)
+    const descs = wBal.map(tkn => `Token:${tkn.tkn.symbol} Balance:${curFormat.format(tkn.bal)}`)
+    return {totalBal, desc:`TotalBal:${curFormat.format(totalBal)}, ${descs.join(', ')}`}
 }
 
-//#endregion
+async function manageOpenOrders(orderList, fromT, toT, size) {
+// we have a few numbers to handle open orders
+// order size: is the size of the order when placed
+// min order size: if remaining order is below 10% of order size then ignore for calculations
+// order cut off: if the total of open order remaining is less than 50 % of order size then place new order
+// function should be called once for each pair
+    remain =  orderList.filter(ord => ord.data.makerAsset === fromT.address && ord.data.takerAsset === toT.address)
+                                .reduce((prev, current) => {const curRemaining = parseFloat(formatUnits(current.remainingMakerAmount, fromT.decimals) )
+                                    return curRemaining > (size/10) ? prev + curRemaining : prev}, 0 )
 
-//#region env functions
+    return remain < size /2 ? {remain, placed:true, ret:(await createLimitOrder(fromT, toT, size, (size * (1+ SPREADBP/10000) ).toString() , DEFTIMEOUT))} : {remain, placed:false}
 
-async function getEnv() {
-    const provider = new ethers.providers.JsonRpcProvider(providerUrl)
-    const walletWithProvider = new ethers.Wallet(privateKey, provider)
-    const dhedge = new Dhedge(walletWithProvider, Network.POLYGON)
-    const p = await dhedge.loadPool(MyPoolStables)
-    const pool = new myPool(dhedge.network, dhedge.signer, p.poolLogic, p.managerLogic, dhedge.utils)
-    return {dhedge, pool}
 }
 
-//#region  One time seup functions
-async function createPool() {
-    const dhedge = await  getEnv()
-    const pool = await dhedge.createPool(
-    "Zinth",
-    "Zinth Fund A",
-    "ZINA",
-    [[USDC, true], [DAI, true], [USDT, true] ],
-    10
-    )
-    console.log("created pool with address", pool.address)
+async function placeRebalOrder(orderList, fromT, toT, size) {
+    return await createLimitOrder(fromT, toT, size, (size.toString() * (1 - SPREADBP/10000/5) ).toString() , DEFTIMEOUT)
 }
 
-async function oneTimePoolSetup(env) {
-   
-    const appr1Tx = await env.pool.approveDeposit(USDC, ethers.constants.MaxUint256)
-    const appr2Tx = await env.pool.approveDeposit(DAI, ethers.constants.MaxUint256)
-    const appr3Tx = await env.pool.approveDeposit(USDT, ethers.constants.MaxUint256)
-    console.log(`sent deposit approve requests: ${appr1Tx.hash}, ${appr2Tx.hash}, ${appr3Tx.hash}`)
-
-    const apprTx1 = await env.pool.approve( Dapp.SUSHISWAP, USDC, ethers.constants.MaxInt256)
-    const apprTx2 = await env.pool.approve( Dapp.SUSHISWAP, DAI, ethers.constants.MaxInt256)
-    const apprTx3 = await env.pool.approve( Dapp.SUSHISWAP, USDT, ethers.constants.MaxInt256) 
-    console.log(`sent trading approve requests: ${apprTx1.hash}, ${apprTx2.hash}, ${apprTx3.hash}`)
-
-   
-}
-
-//#endregion
-
-//#region core logic
-
-async function getCurrentPosn(env, tokens) {
-
-    const comp =  await env.pool.getComposition()
-
-    return tokens.map(item => {
-        let tknBal = comp.filter(compItem => (item.address.toLowerCase() === compItem.asset.toLowerCase()))
-        return {...item, bal:denomToUnit(tknBal[0].balance.toString(), item)}
+function getOpenOrdersSummary(orderList) {
+    const ordDescs = orderList.map(ord => {
+        const fromT = symLookup[ord.data.makerAsset]
+        const toT = symLookup[ord.data.takerAsset]
+        return `From: ${fromT.symbol} To: ${toT.symbol} remaining: ${formatUnits(ord.remainingMakerAmount,fromT.decimals)} at create time:${ord.createDateTime}`
     })
-
+    return ordDescs.join('\n')
 }
 
-async function stablesStep(env){
-    const tradeSize = TRADE_SIZE
-    const fastGas = await getFastGas()
-    const tokens = [findTkn('USDC'), findTkn('DAI'), findTkn('USDT')]
+async function mainLoop(env, bal){ //bal = {startbal:100, curBal}
+    const allTkns = [USDC, DAI, USDT]
+    const curBal = await getBalsAndDesc(allTkns, env)
+    console.log(curBal.desc)
+    const orderList = await getOpenLimitOrders()
+    console.log(getOpenOrdersSummary(orderList))
 
-    const tokensBal = await getCurrentPosn (env, tokens)
-    const totalBal = tokensBal.reduce((acc, curr) => acc + parseFloat(curr.bal),0)
-    const balLimit= totalBal /2
+    //for each currency pair manage open orders. (if not enough open then place new)
+    const tknPairs = [[ USDC, DAI], [USDC,USDT], [DAI,USDT]]
+    const tknMgtResults = await Promise.all(tknPairs.map(async (tkns) => {
+        return {tkn1Mng: await manageOpenOrders(orderList, tkns[0], tkns[1], TRADE_SIZE),
+                tkn2Mng: await manageOpenOrders(orderList, tkns[1], tkns[0], TRADE_SIZE) }}))
 
-
-    const quotes = await sushiPrice.getSushiPrices(tokensBal,tradeSize)
-
-    // remove quotes where from balance is below trade size and where to balance is greater then half of pool
-    const balQuotes = quotes.filter(item=> (item.pair.from.bal > tradeSize && item.pair.to.bal < balLimit) )
-
-    const maxQuote = balQuotes.reduce((prev, curr)=> prev.priceRes.price > curr.priceRes.price ? prev : curr)
-    if (maxQuote.priceRes.price > 1) {// trade it
-        console.log(`trading from ${maxQuote.pair.from.symbol} to ${maxQuote.pair.to.symbol}`)
-        const fromAmt = unitToDenom(tradeSize, maxQuote.pair.from)
-        const toAmt = unitToDenom(tradeSize, maxQuote.pair.to)
-        await env.pool.trade(Dapp.SUSHISWAP, maxQuote.pair.from.address, maxQuote.pair.to.address, fromAmt, toAmt, fastGas)
-    }
 }
 
 function sleep(ms) {return new Promise(resolve => setTimeout(resolve, ms))}
 
-async function runMany(num, msDelay, f) {
-    let env = await getEnv()
+async function runManyParam(num, msDelay, f, init, param) {
     for (let i = 0; i < num; i++) {
         let delay = msDelay
-        try {await f(env)}
-        catch (err) {console.log(`ERROR Found: ${err}`); delay = delay * 2}
-        console.log(`done loop ${i}`)
-        await sleep(delay)
-    }
-}
-
-
-async function runManyParam(num, msDelay, f, initParam) {
-    let param = initParam
-    for (let i = 0; i < num; i++) {
-        let delay = msDelay
-        try {param = await f(param)}
+        try {param = await f(init, param)}
         catch (err) {console.log(`ERROR Found: ${err}`); delay = delay * 2}
         console.log(`done loop ${i}`)
         await sleep(delay)
@@ -159,7 +171,4 @@ async function runManyParam(num, msDelay, f, initParam) {
     return param
 }
 
-
-//#endregion
-
-runMany(process.env.NUM_OF_LOOPS, process.env.LOOP_TIME_MS,stablesStep)
+//runManyParam(NUM_OF_LOOPS, LOOP_TIME_MS, mainLoop,await getEnv(privateKey), {})
